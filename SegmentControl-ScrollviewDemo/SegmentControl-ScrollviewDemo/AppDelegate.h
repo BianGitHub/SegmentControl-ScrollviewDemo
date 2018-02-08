ca3b6743e0f25341e85601f1bc952e01cd8e9204
边雷 <bianlei@csii.com.cn>
2018-02-08 11:27:50 +0800

    创建项目

---
+++ SegmentControl-ScrollviewDemo/SegmentControl-ScrollviewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SegmentControl-ScrollviewDemo
+//
+//  Created by 边雷 on 2018/2/8.
+//  Copyright © 2018年 Mac-b. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SegmentControl-ScrollviewDemo/SegmentControl-ScrollviewDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SegmentControl-ScrollviewDemo
+//
+//  Created by 边雷 on 2018/2/8.
+//  Copyright © 2018年 Mac-b. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor lightGrayColor];
+}
+
+@end
