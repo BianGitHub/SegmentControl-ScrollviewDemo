d6743bdc9ec765b097b9544332c2e1b36b0ec11b
边雷 <bianlei@csii.com.cn>
2018-02-08 11:40:07 +0800

    setUI

--- SegmentControl-ScrollviewDemo/SegmentControl-ScrollviewDemo/ViewController.m
+++ SegmentControl-ScrollviewDemo/SegmentControl-ScrollviewDemo/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define SCREEN_WIDTH self.view.bounds.size.width
+#define SCREEN_HEIGHT self.view.bounds.size.height
 
+@interface ViewController () <UIScrollViewDelegate>
+@property (nonatomic, strong) UIScrollView *scrollview;
 @end
 
 @implementation ViewController
@@ -17,7 +20,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor lightGrayColor];
+//    self.view.backgroundColor = [UIColor lightGrayColor];
+    
+    //
+    [self setUI];
+}
+
+#pragma mark - setUI
+- (void)setUI {
+    
+    NSArray *arr = @[@"登录一",@"登录二"];
+    
+    // 创建segmentControl
+    UISegmentedControl *segmentC = [[UISegmentedControl alloc] initWithItems:arr];
+    segmentC.frame = CGRectMake(20, 20, SCREEN_WIDTH - 20 * 2, 44);
+    segmentC.selectedSegmentIndex = 0;//设置默认选择项索引
+    [segmentC addTarget:self action:@selector(segmentCAction:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:segmentC];
+    
+    // 创建scrollview
+    self.scrollview = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 64,SCREEN_WIDTH, SCREEN_HEIGHT-64)];
+    self.scrollview.backgroundColor = [UIColor redColor];
+    self.scrollview.delegate = self;
+    self.scrollview.alwaysBounceHorizontal = YES;
+    self.scrollview.contentSize = CGSizeMake(self.view.bounds.size.width * 2, self.view.bounds.size.height-164);
+    self.scrollview.pagingEnabled = YES;
+    self.scrollview.bounces = NO;
+    [self.view addSubview:self.scrollview];
+}
+
+#pragma mark - segmentCAction
+- (void)segmentCAction:(UISegmentedControl *)segmentC {
+    NSLog(@"%zd", segmentC.selectedSegmentIndex);
 }
 
 @end
